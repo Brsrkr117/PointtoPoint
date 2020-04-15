07bdac0f387a2649623475ed91e03deb417e3032
Brsrkr117 <54762821+Brsrkr117@users.noreply.github.com>
2020-04-15 18:55:38 +0530

    orders can be placed

--- app/src/main/java/com/example/pointtopoint/ItemListActivity.java
+++ app/src/main/java/com/example/pointtopoint/ItemListActivity.java
@@ -1,6 +1,7 @@
 package com.example.pointtopoint;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,8 +20,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ItemListActivity extends AppCompatActivity {
 
     ListView listView;
-    String mTitle[] = {"Non Veg", "Non veg", "Non Veg", "Veg", "Veg","Veg"};
-    String mDescription[] = {"Small", "Medium", "Large", "Small", "Medium", "Large"};
+    String mTitle[] = {"Non Veg Small", "Non veg Medium", "Non Veg Large", "Veg Small", "Veg Medium","Veg Large"};
+    String mDescription[] = {"60", "70", "80", "50", "60", "70"};
     int images[] = {R.drawable.shopping_cart,R.drawable.shopping_cart, R.drawable.shopping_cart, R.drawable.shopping_cart, R.drawable.shopping_cart, R.drawable.shopping_cart};
 
     @Override
@@ -38,20 +39,85 @@ public class ItemListActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if (position ==  0) {
-                    Toast.makeText(ItemListActivity.this, "Facebook Description", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ItemListActivity.this, "You have selected Non Veg Small", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), OrderViewActivity.class);
+                    Bundle bundle = new Bundle();
+                    //bundle.putInt("position",0);
+                    bundle.putString("title",mTitle[0]);
+                    bundle.putString("description", mDescription[0]);
+                    intent.putExtras(bundle);
+                    intent.putExtra("title", mTitle[0]);
+                    intent.putExtra("description", mDescription[0]);
+                    intent.putExtra("position", ""+0);
+                    startActivity(intent);
+
+
                 }
                 if (position ==  1) {
-                    Toast.makeText(ItemListActivity.this, "Whatsapp Description", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ItemListActivity.this, "You have selected Non veg Medium", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), OrderViewActivity.class);
+                    Bundle bundle = new Bundle();
+                    //bundle.putInt("position",1);
+                    bundle.putString("title",mTitle[1]);
+                    bundle.putString("description", mDescription[1]);
+                    intent.putExtras(bundle);
+                    intent.putExtra("title", mTitle[1]);
+                    intent.putExtra("description", mDescription[1]);
+                    intent.putExtra("position", ""+1);
+                    startActivity(intent);
                 }
                 if (position ==  2) {
-                    Toast.makeText(ItemListActivity.this, "Twitter Description", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ItemListActivity.this, "You have selected Non Veg Large", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), OrderViewActivity.class);
+                    Bundle bundle = new Bundle();
+                    //bundle.putInt("position",2);
+                    bundle.putString("title",mTitle[2]);
+                    bundle.putString("description", mDescription[2]);
+                    intent.putExtras(bundle);
+                    intent.putExtra("title", mTitle[2]);
+                    intent.putExtra("description", mDescription[2]);
+                    intent.putExtra("position", ""+2);
+                    startActivity(intent);
                 }
                 if (position ==  3) {
-                    Toast.makeText(ItemListActivity.this, "Instagram Description", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ItemListActivity.this, "You have selected Veg Small", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), OrderViewActivity.class);
+                    Bundle bundle = new Bundle();
+                    //bundle.putInt("position",3);
+                    bundle.putString("title",mTitle[3]);
+                    bundle.putString("description", mDescription[3]);
+                    intent.putExtras(bundle);
+                    intent.putExtra("title", mTitle[3]);
+                    intent.putExtra("description", mDescription[3]);
+                    intent.putExtra("position", ""+3);
+                    startActivity(intent);
                 }
                 if (position ==  4) {
-                    Toast.makeText(ItemListActivity.this, "Youtube Description", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ItemListActivity.this, "You have selected Veg Medium", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), OrderViewActivity.class);
+                    Bundle bundle = new Bundle();
+                    //bundle.putInt("position",4);
+                    bundle.putString("title",mTitle[4]);
+                    bundle.putString("description", mDescription[4]);
+                    intent.putExtras(bundle);
+                    intent.putExtra("title", mTitle[4]);
+                    intent.putExtra("description", mDescription[4]);
+                    intent.putExtra("position", ""+4);
+                    startActivity(intent);
                 }
+                if (position ==  5) {
+                    Toast.makeText(ItemListActivity.this, "You have selected Veg Large", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), OrderViewActivity.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("title",mTitle[5]);
+                    bundle.putString("description", mDescription[5]);
+                    intent.putExtras(bundle);
+                    intent.putExtra("title", mTitle[5]);
+                    intent.putExtra("description", mDescription[5]);
+                    intent.putExtra("position", ""+5);
+                    startActivity(intent);
+                }
+
             }
         });
 
@@ -83,7 +149,7 @@ public class ItemListActivity extends AppCompatActivity {
             TextView myDescription = row.findViewById(R.id.textView2);
             images.setImageResource(rImgs[position]);
             myTitle.setText(rTitle[position]);
-            myDescription.setText(rDescription[position]);
+            myDescription.setText("Rs" + rDescription[position]);
             return row;
         }
     }
---
+++ app/src/main/java/com/example/pointtopoint/OrderViewActivity.java
@@ -0,0 +1,89 @@
+package com.example.pointtopoint;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OrderViewActivity extends AppCompatActivity {
+    //ImageView imageView;
+    private TextView title, description;
+    private int position;
+    private Button placeorder,goback;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore db;
+    private String UserID;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_view);
+
+        title = findViewById(R.id.titleText);
+        description = findViewById(R.id.descriptionText);
+        goback = (Button) findViewById(R.id.btngoback);
+        placeorder = (Button) findViewById(R.id.btnPlaceOrder);
+
+        db= FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        db=FirebaseFirestore.getInstance();
+
+        Intent intent = getIntent();
+        Bundle bundle = this.getIntent().getExtras();
+        final String aTitle = intent.getStringExtra("title");
+        final String aDescription = intent.getStringExtra("description");
+
+        title.setText(aTitle);
+        description.setText("Rs" + aDescription + "\n+Additional Delivery Charges");
+
+
+        placeorder.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+                UserID=firebaseAuth.getCurrentUser().getUid();
+                Map<String, Object> order = new HashMap<>();
+                order.putIfAbsent("Type of Order",aTitle);
+                order.putIfAbsent("Price",aDescription);
+                db.collection("orders").add(order).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(OrderViewActivity.this, "Select Pick-up location", Toast.LENGTH_LONG).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(OrderViewActivity.this, "Order cannot be placed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+        goback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(OrderViewActivity.this, "Order Cancelled", Toast.LENGTH_SHORT).show();
+                finish();
+                startActivity(new Intent(OrderViewActivity.this,ItemListActivity.class));
+            }
+        });
+    }
+}
