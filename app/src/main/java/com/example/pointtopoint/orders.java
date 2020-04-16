e4ffa6c258fda6297dddb373c6ab65d41a3e6a24
Brsrkr117 <54762821+Brsrkr117@users.noreply.github.com>
2020-04-16 16:47:32 +0530

    user details for order added

--- app/src/main/java/com/example/pointtopoint/orders.java
+++ app/src/main/java/com/example/pointtopoint/orders.java
@@ -4,16 +4,68 @@ public class orders {
     String orderid;
     String ordertype;
     String price;
+    String userfullname;
+    String userid;
+    String username;
+    String useremail;
+    String usernumber;
+
+    public String getUserfullname() {
+        return userfullname;
+    }
+
+    public void setUserfullname(String userfullname) {
+        this.userfullname = userfullname;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUseremail() {
+        return useremail;
+    }
+
+    public void setUseremail(String useremail) {
+        this.useremail = useremail;
+    }
+
+    public String getUsernumber() {
+        return usernumber;
+    }
+
+    public void setUsernumber(String usernumber) {
+        this.usernumber = usernumber;
+    }
+
+
 
 
     public orders(){
 
     };
 
-    public orders(String orderid, String ordertype, String price) {
+    public orders(String orderid, String ordertype, String price, String userfullname, String userid, String username, String useremail, String usernumber) {
         this.orderid = orderid;
         this.ordertype = ordertype;
         this.price = price;
+        this.userfullname = userfullname;
+        this.userid = userid;
+        this.username = username;
+        this.useremail = useremail;
+        this.usernumber = usernumber;
     }
 
     public String getOrderid() {
